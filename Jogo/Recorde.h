2763ee7e7c10efc3b578981075e5b3dd72c49d22
felipebernardo10 <felipebernardo895@gmail.com>
2025-10-03 21:56:13 -0300

    Add files via upload

---
+++ Jogo/Recorde.h
@@ -0,0 +1,64 @@
+#pragma once
+
+
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+class Recorde {
+private:
+    string nome;
+    int pontos;
+    string arquivo;
+
+public:
+    Recorde(string nomeArquivo = "record.txt") {
+        arquivo = nomeArquivo;
+        nome = "Ninguém";
+        pontos = 0;
+        carregar();
+    }
+
+    void carregar() {
+        ifstream file(arquivo);
+        if (file.is_open()) {
+            file >> pontos;
+            file.ignore();
+            getline(file, nome);
+            file.close();
+        }
+    }
+
+    void salvar() {
+        ofstream file(arquivo);
+        if (file.is_open()) {
+            file << pontos << endl;
+            file << nome << endl;
+            file.close();
+        }
+    }
+
+    void mostrar() const {
+        cout << "Recorde atual: " << pontos << " (" << nome << ")" << endl;
+    }
+
+    void verificarNovoRecorde(int score) {
+        cout << "\nSua pontuação: " << score << endl;
+        if (score > pontos) {
+            cout << "🎉 Parabéns! Você bateu o recorde! 🎉" << endl;
+            cout << "Digite seu nome: ";
+            string novoNome;
+            getline(cin, novoNome);
+
+            pontos = score;
+            nome = novoNome;
+            salvar();
+            cout << "Novo recorde salvo com sucesso!" << endl;
+        } else {
+            cout << "Você não bateu o recorde. Tente novamente!" << endl;
+        }
+    }
+};
+
+
